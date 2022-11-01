ba5449554d776127698f074ec89b7834f8368d8e
Janberk Cengiz <38561275+JanberkCengiz@users.noreply.github.com>
2022-11-01 19:48:42 +0300

    Add files via upload

---
+++ Başkent.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	char baskent;
+	printf("Türkiyenin başkenti neresidir?");
+	printf("\n==>");
+	scanf("%d",&baskent);
+	if (baskent=="Ankara" || baskent=="ankara");
+		printf("Doğru bildin Türkiyenin başkenti Ankaradır");
+	
+}
